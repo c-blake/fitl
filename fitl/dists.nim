cd06aed3d933c4b81a3a985b2d274e0828fe1d8b
Charles Blake <charlechaud@gmail.com>
2023-10-20 15:11:43 -0400

    Harden `newton` to stay on support; Elim no longer needed `p` capture..
    Inject `p` into scope for PDn usages so that numerical inverse distros
    with finicky initial guess requirements can provide expressions, but
    default to the old 0.125.
    
    Lift out dBeta22 & just use the cubic equation solution for qtl func.
    
    Harden Matterhorn which was never broken - merely narrow as noted now
    in the main program.
    
    Fix a couple `modes` in float32 mode in Nim-1.6 that needed T()s.
    
    Correct Caliper CDF and use a straight line guess p(x) for qtl calc.
    which works fine outside [-0.15, 0.15] (but not within 0.05 of the
    two peaks).
    
    In main test program logic, add 2 new kinds of plot (CDF & Quantile) for
    debugging/validation and change to only cross-check *requested* distro
    when given `--x` of != 0.875 (less likely than 0.25).  Put in Zsh e.g.
    to cross-check all the distros.
    
    Only SmoothComb & Sawtooth have buggy quantile functions (which sample
    fine since they are pure mixtures of things *without* buggy quantiles
    and mixtures sample with 2-stage selection not PIT-based inverse xfm).
    The new guess(p) arrangement allows fixing this, though.

--- fitl/dists.nim
+++ fitl/dists.nim
@@ -9,13 +9,13 @@ type
     support: seq[T],              ## where non-zero (for plots, num.integ., etc.
     modes  : seq[T]]              ## Locations of modes (local maxima)
 
-template newton(p0, cdf, pdf, support, x0, fTol) {.dirty.} = # ?df must use `x`!
-  let p = p0          # Capture in case p0 is an expression like `rand(1.0)`
+template newton(p, cdf, pdf, support, x0, fTol) {.dirty.}= # [pc]df must use `x`
   var x = x0          # Newton's method should be an ok inverter/solver here.
   for it in 1..50:    # Quadratically cvgent. If 50 fails,likely nothing works
     let pr = (cdf) - p
     if abs(pr) < fTol and it > 1: return x
     x -= pr / (pdf)
+    x = min(max(x, support[0]), support[^1])  # Keep bounded on support[]
   result = if p < T(0.5): support[0] else: support[^1]
 
 proc mix*(compons: seq[(T, CDist, T, T)]; support: seq[T] = @[],
@@ -59,18 +59,19 @@ const z=T(0); const mH=T(-0.5); const pQ=T(0.25); const m6=T(-6);const p2=T(2)
 const o=T(1); const pH=T(+0.5); const p4=T(+4)  ; const p6=T(+6);const oT=T(0.1)
 const p20 = T(20); const o3=T(1)/T(3) #z)ero,o)ne,mX=-X, pX=+X; oT=(o)ne(T)enth
 
-template PD(pd, cd, qt, supp, mo): untyped =    # For invertible formula distros
+template PD(pd, cd, qt, supp, mo): untyped =    # Invertible formula P.Distros
   ((proc(x {.inject.}: T): T {.closure.} = pd),
    (proc(x {.inject.}: T): T {.closure.} = cd),
    (proc(p {.inject.}: T): T {.closure.} = qt),
    (proc(): T {.closure.} = (let p {.inject.} = rand(1.0); qt)),
    supp, mo)
 
-template PDn(pd, cd, supp, mo): untyped =       # For numerical invert distros
+template PDn(pd, cd, supp, mo; guess: untyped=0.125): untyped = # Numer.Inverse
   ((proc(x {.inject.}: T): T {.closure.} = pd), # expr in terms of `x`
    (proc(x {.inject.}: T): T {.closure.} = cd), # expr in terms of `x`
-   (proc(p: T): T {.closure.} = newton(p, cd, pd, supp, 0.125, 5e-6)),
-   (proc(): T {.closure.} = newton(rand(1.0), cd, pd, supp, 0.125, 5e-6)),
+   (proc(p {.inject.}: T): T {.closure.} = newton(p, cd, pd, supp, guess,2e-6)),
+   (proc(): T {.closure.} =
+     let p {.inject.} = rand(1.0); newton(p, cd, pd, supp, guess, 2e-6)),
    supp, mo)
 
 # Define common `CDist`s;  Export so importers can do their own mixtures/etc.
@@ -99,6 +100,14 @@ let dTri* = PD(
 let dLogNormal* = PD(gauss.pdf[T](ln(x))/x, gauss.cdf[T](ln(x)),
                      exp(gauss.qtl[T](p)), @[z, p20], @[exp(-o)])
 
+let dBeta22* = PD(6*x*(1-x), (3-2*x)*x*x, # 2x^3-3x^2+0x+p=0; a=2,b=-3,c=0,d=p
+                    (let P   = T(-0.75);    # (3ac - b^2)/(3a^2)
+                     let Q   = -pQ + pH*p;  # (2b^3 - 9abc + 27da^2)/(27a^3)
+                     let rp3 = sqrt(-P*o3); # wikipedia.org/wiki/Cubic_equation
+                     let t1  = p2*rp3*cos(o3*arccos(Q/(o3*p2*P*rp3))-2*PI*o3*1);
+                     t1 + T(0.5)),          # xk = tk - b/(3a); k=1 middle root
+                    @[z, o], @[pH])
+
 let distros* = {"U01":dU, "Exp": dExp,  #TODO Check supports&modes; Maybe plot?
   "Maxwell": PD(x*exp(mH*x*x), o-exp(mH*x*x), sqrt(-p2*ln(o-p)), @[m6,p6],@[o]),
   "Laplace": dLaplace,
@@ -113,12 +122,11 @@ let distros* = {"U01":dU, "Exp": dExp,  #TODO Check supports&modes; Maybe plot?
                     else    : o/(o-abs(p2*p-o))^2-o, @[-p20, p20], @[z]),
   "N01": dN, "logNormal": dLogNormal,
   "3BinHisto1" : mix(@[(pH,dU,mH,o), (pH,dU,T(-5),T(10))], modes = @[z]),
-  "Matterhorn" : PD(o/(x.abs*ln(x.abs)^2),
+  "Matterhorn" : PD(if abs(x) > exp(-p2): 0.0 else: o/(x.abs*ln(x.abs)^2),
     if x.abs < -exp(-p2): z elif x.abs<exp(-p2): pH - x.sgn.T/x.abs.ln else: o,
     (let P = p-pH; let PS = P.sgn.T; PS*exp(-PS/P)),
     @[-exp(-p2), -1e-3, +1e-3, exp(-p2)], @[z]),
-  "Log"        : PDn(-ln(x), x - x*ln(x), @[z, o], @[z]), "Triangle": dTri,
-  "Beta2,2"    : PDn(6*x*(1-x), (3-2*x)*x*x, @[z, o], @[pH]),
+  "Log": PDn(-ln(x), x-x*ln(x), @[z,o], @[z]),"Triangle":dTri,"Beta2,2":dBeta22,
   "ChiSquare1" : PDn(exp(-pH*x)/sqrt(2*PI*x), erf(sqrt(pH*x)), @[z, p20], @[z]),
   "NormalCubed": PD(gauss.pdf[T](x.sgn.T*pow(x.abs, o3))*o3*pow(x^2,-o3),
                     gauss.cdf[T](x.sgn.T*pow(x.abs, o3)),
@@ -127,7 +135,7 @@ let distros* = {"U01":dU, "Exp": dExp,  #TODO Check supports&modes; Maybe plot?
                     o/ln(p)^2, @[z, p20], @[o/T(9)]),
   "Marronite"    : mix(@[(p2/T(3), dN,z,o), (o/T(3), dN,-p20,o/T(4))]),
   "SkewedBimodal": mix(@[(T(0.75), dN,z,o), (o/p4, dN,T(1.5),o/T(3))],
-                     modes = @[0.0005447, 1.442525]), #e^-x^2/2+e^-9/2*(x-3/2)^2
+              modes = @[T(0.0005447), T(1.442525)]), #e^-x^2/2+e^-9/2*(x-3/2)^2
   "Claw"         : mix(@[(pH, dN,z,o ), (oT, dN,-o,oT), (oT, dN,mH,oT),
                          (oT, dN,z,oT), (oT, dN,pH,oT), (oT, dN,o ,oT)]),
   "SmoothComb"   : mix(@[(T(32)/T(63), dN,T(-31)/T(21), T(32)/T(63)),
@@ -141,8 +149,8 @@ let distros* = {"U01":dU, "Exp": dExp,  #TODO Check supports&modes; Maybe plot?
     if   x < -T(1.1): z
     elif x < -oT    : pH*(o-(p4*(x.abs-oT)-3*pow(abs(-x-oT), p4*o3)))
     elif x < oT     : pH
-    elif x < T(1.1) : 0.5*(4*(x-0.1)-3*pow(abs(x-0.1), p4*o3))
-    else: 1.0, @[-T(1.1), -oT, oT, T(1.1)], @[-oT, oT]),
+    elif x < T(1.1) : pH + 0.5*(4*(x-0.1)-3*pow(abs(x-0.1), p4*o3))
+    else: 1.0, @[-T(1.1), -oT, oT, T(1.1)], @[-oT, oT], T(-1.09) + p*T(2.08)),
   "TriModeU": mix(@[(pQ,dU,T(-20.1),oT), (pH,dU,-o,p2), (pQ,dU,p20,oT)]),
   "Sawtooth": mix(@[(oT, dTri,T(-9),o), (oT, dTri,T(-7),o), (oT, dTri,T(-5),o),
                     (oT, dTri,T(-3),o), (oT, dTri,T(-1),o), (oT, dTri,T(+1),o),
@@ -209,28 +217,29 @@ proc match*(ds: openArray[(string, CDist)], pfx: string): int =
 when isMainModule:      # A trivial command line driver mostly for testing.
   import cligen; when defined(release): randomize()
   template C(a,b,th):untyped = a.abs>th and abs(a-b)>th and abs(a/b-1)>sqrt(th)
-  proc dists(distro="U01", nSamp=0, x=0.25, plot=false, modes=false, v=false) =
+  type Plot = enum pNone="none", pPDF="pdf", pCDF="cdf", pQtl="qtl"
+  proc dists(distro="U01", nSamp=0, x=0.875, plot=pNone, mode=false, v=false) =
     var dno = 0
     try: dno = distros.match(distro)
     except IOError as e: echo e.msg; quit(1)
     if v: stderr.write "Distro[", dno+1, "]: ", distros[dno][0], '\n'
-    let dist = distros[dno][1]
-    for i in 1..nSamp: echo dist.gen()
-    if x != 0.25:       # All Ds != 0 @0.125; Whole domain test is out of scope
+    let (nm,dist) = distros[dno]; let (pdf, cdf, qtl, gen, support, modes)=dist
+    for i in 1..nSamp: echo gen()
+    if x != 0.875:      # All Ds != 0 @0.125; Whole domain test is out of scope
       let h = 5e-5      # for numerical derivative of CDF
-      for (nm, distro) in distros:
-        let (pdf, cdf, qtl, _, _, _) = distro
-        let p = pdf(x)
-        if p > 0:
-          let c = cdf(x); let q = qtl(c)
-          let dc = (cdf(x + h) - c)/h
-          if C(p/dc, 1, 1e-4): echo nm," dCdf!=pdf; x: ",x," pd: ",p," dc: ",dc
-          if C(q   , x, 1e-4): echo nm," qtl(c)!=I; x: ",x," cd: ",c," qt: ",q
-    if plot:
-      let scl = (dist.support[^1] - dist.support[0])/4096.0
-      for i in 0..4095:
-        let x = dist.support[0] + i.T * scl
-        echo x, " ", if x > dist.support[0]: dist.pdf(x) else: 0.0
-      echo dist.support[^1], " ", 0.0
-    if modes: (for m in dist.modes: echo m)
+      let p = pdf(x)    # Below tests ok, but narrower range for e.g. Matterhorn
+      if p > 0f: #for D in {1..35};{for X in {0..20};dists -d$D -x$[(X-10)*0.1]}
+        let c = cdf(x); let q = qtl(c)
+        let dc = (cdf(x + h) - c)/h
+        if C(p/dc, 1, 1e-4): echo nm," dCdf!=pdf; x: ",x," pd: ",p," dc: ",dc
+        if C(q   , x, 1e-4): echo nm," qtl(c)!=I; x: ",x," cd: ",c," qt: ",q
+    if plot in {pPDF, pCDF}:
+      let scl = (support[^1] - support[0])/4096.0
+      for i in 1..4095:
+        let x = support[0] + i.T * scl
+        echo x, " ", if plot == pCDF: cdf(x) else: pdf(x)
+      echo support[^1], " ", 0f
+    elif plot == pQtl:
+      for i in 0..1024: (let x = i.float*0.0009765625; echo x," ",qtl(x))
+    if mode: (for m in modes: echo m)
   dispatch dists
