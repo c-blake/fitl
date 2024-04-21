76892b8ffb93b4b858f1da927313b1fb3d3a683d
Charles Blake <charlechaud@gmail.com>
2024-04-21 08:17:30 -0400

    Pass y-offset `yO` to avoid allocation for autocorrelations.

--- fitl/covar.nim
+++ fitl/covar.nim
@@ -3,22 +3,22 @@ import basicLA; from std/math import sqrt
 
 proc mean[F](x: ptr F; n: int): F = sum(x, n) / F(n)
 
-proc corr[F](x, y: openArray[F]; xm, ym: F; n: int): F =
+proc corr[F](x, y: openArray[F]; xm, ym: F; n: int; yO=0): F =
   var syy, sxy, sxx: F
   for i in 0 ..< n:
-    sxx += (x[i] - xm)*(x[i] - xm)
-    syy += (y[i] - ym)*(y[i] - ym)
-    sxy += (x[i] - xm)*(y[i] - ym)
+    sxx += (x[i]    - xm)*(x[i]    - xm)
+    syy += (y[yO+i] - ym)*(y[yO+i] - ym)
+    sxy += (x[i]    - xm)*(y[yO+i] - ym)
   sxy / sqrt(sxx * syy)
 
-proc corr*[F](x, y: openArray[F]): F =
+proc corr*[F](x, y: openArray[F], yO=0): F =
   ## Return Pearson linear correlation coefficient between x & y (over leading
   ## elements if one array is longer).
   let n = min(x.len, y.len)
-  corr x, y, mean(x[0].unsafeAddr, n), mean(y[0].unsafeAddr, n), n
+  corr x, y, mean(x[0].unsafeAddr, n), mean(y[yO].unsafeAddr, n), n, yO
 
 proc corrAuto*[F](x: openArray[F], lag=1): F =
-  if lag < x.len: corr(x, x[lag..^1]) else: F(0)
+  if lag < x.len: corr(x, x, lag) else: F(0)
 
 proc covMat*[F](v: var openArray[F]; x: openArray[F]; n, m: int) =
   ## Save in `v` usual symmetric m*m Covariance matrix for n*m input matrix `x`
